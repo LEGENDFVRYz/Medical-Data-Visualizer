071f298fe6863915142fdcf50c18f25c8e704a83
Sebastien Louis Cruz <cruzsebastienalt28@gmail.com>
2024-02-27 18:37:30 +0000

    Changes to be committed: 	modified:   medical_data_visualizer.py
        Note: First Chart was completed, The second one is ongoing...

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -14,7 +14,8 @@ df['overweight'] = (df['overweight'] > 25).astype(int)              # Check if o
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df['cholesterol'] = (df['cholesterol'] > 1).astype(int)     
 df['gluc'] = (df['gluc'] > 1).astype(int)                   
-df.head(10)                                                 # Checking if the was normalized correctly
+df.head(10)         # Checking if the was normalized correctly
+
 
 # Draw Categorical Plot
 def draw_cat_plot():
@@ -26,18 +27,22 @@ def draw_cat_plot():
         var_name = 'variable'
     )
 
-
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    df_cat = pd.DataFrame(df_cat.groupby(['cardio', 'variable']).value_counts()).reset_index()
+    df_cat.rename(columns={0: 'total'}, inplace=True)       # rename the missing column title after counting by
+
 
     # Draw the catplot with 'sns.catplot()'
-
-
+    cat = sns.catplot(
+        data = df_cat,
+        kind = 'bar',               # Type of the graph
+        col = 'cardio',             # Split by cardio, so 2 charts will be the output
+        x='variable', y='total',    # X and Y values
+        hue = 'value'               # 'value' will be reference for the color
+    )
 
     # Get the figure for the output
-    fig = None
-
+    fig = cat.fig
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
