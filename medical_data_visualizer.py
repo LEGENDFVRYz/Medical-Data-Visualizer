4e1744f620c5f6da1a33ca99907afaa850f14136
Sebastien Louis Cruz <cruzsebastienalt28@gmail.com>
2024-02-28 13:44:58 +0000

    Changes to be committed: 	modified:   heatmap.png 	modified:   medical_data_visualizer.py
        Note: Solution was completed!

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -52,21 +52,36 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[df['ap_lo'] <= df['ap_hi']]                        # diastolic pressure is higher than systolic
+    df_heat = df_heat[df['height'] >= df['height'].quantile(0.025)] # height is less than the 2.5th percentile
+    df_heat = df_heat[df['height'] <= df['height'].quantile(0.975)] # height is more than the 97.5th percentile
+    df_heat = df_heat[df['weight'] >= df['weight'].quantile(0.025)] # weight is less than the 2.5th percentile
+    df_heat = df_heat[df['weight'] <= df['weight'].quantile(0.975)] # weight is more than the 97.5th percentile
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
-
+    mask = np.ones_like(df_heat.corr(), dtype=bool)   # Create a copy of corr where all values is ones
+    mask = np.triu(mask, 0)                           # mask out correct portion of the triangle
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    sns.heatmap(
+        data = corr,                    # import data
+        mask = mask,                    # masking the heatmap to look like a triangle
+        vmax = 0.30,                    # max val for colors
+        annot = True,                   # show text in each boxes
+        annot_kws = {'fontsize' : 5},   # modifying fontsize
+        fmt=".1f",                      # format annot in one digit after the decimal point
+        linewidths = 0.2,               # line width that closely to reference
+        linecolor = 'white',            # color white to make it look like a gap
+        cbar_kws = {"shrink" : .5},     # shrink color bar
+        square = True                   # making sure each boxes is square
+    )
 
 
     # Do not modify the next two lines
